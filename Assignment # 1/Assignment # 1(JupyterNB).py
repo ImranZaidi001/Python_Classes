f05b3dc189c537187a32324db8e9bf44810bdfe3
Imran Haider <141459564+ImranZaidi001@users.noreply.github.com>
2023-08-23 23:01:48 +0500

    Add files via upload

---
+++ Assignment # 1/Assignment # 1(JupyterNB).py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # String Concatination & Formating
+
+# In[15]:
+
+
+Name ="Imran Haider"
+Class = "Data Science"
+Uni = "NED University"
+
+string=Name+_+"Studying in"+_+Class+_+"From"+_+Uni
+print(string)
+
+
+# In[ ]:
+
+
+
+
---
+++ Assignment # 1/Assignment # 1(VScode).py
@@ -0,0 +1,6 @@
+Name ="Imran Haider"
+Class = "Data Science"
+Uni = "NED University"
+_ = " "
+string=Name+_+"Studying in"+_+Class+_+"From"+_+Uni
+print(string)
